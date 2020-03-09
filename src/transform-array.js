55f70ce8c67b01f1ffaed89a4161457fc82b8855
Prokopenko <alex27prok@gmail.com>
2020-03-10 02:38:11 +0300

    feat: Start to create transform-array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,20 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
+module.exports = function transform(arr) {
+    //throw 'Not implemented';
     // remove line with error and write your code here
+
+    if(Array.isArray(arr))
+    {
+        var history = [];
+        for(var i = 0; i < arr.length;i++)
+        {
+            if(arr[i] == '--discard-next') 
+            if(arr[i] == '--discard-prev')
+            if(arr[i] == '--double-next')
+            if(arr[i] == '--double-prev')
+        }
+
+    }
+    else 
+        throw new Error();
+
 };
