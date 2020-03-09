2676a4bbcc32fae3d00df92050e57b148bdd9e78
Prokopenko <alex27prok@gmail.com>
2020-03-10 02:12:24 +0300

    fix: Add towel-hanoi function

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,13 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    //throw 'Not implemented';
     // remove line with error and write your code here
+    var steps = Math.pow(2,disksNumber) - 1; //steps for this algorithm are (2^n - 1), where n is number of circles
+    var time = steps / (turnsSpeed/ 3600); //we have speed per hour? but we should send result as seconds
+
+    var k = {
+        turns: steps,
+        seconds: time
+    }
+
+    return k;
 }
